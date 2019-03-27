b8bf119902cb62d0c0515b77ef5ab1476b748aa2
Marc Davies <marcdaviesriot@gmail.com>
2019-03-27 01:19:24 -0700

    Add function to reveal definition on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-$(document).ready() {
-
-};
+$(document).ready(function() {
+  $("p#show1").click(function() {
+    $("p#reveal1").toggle();
+  });
+  $("p#show2").click(function() {
+    $("p#reveal2").toggle();
+  });
+  $("p#show3").click(function() {
+    $("p#reveal3").toggle();
+  });
+  $("p#show4").click(function() {
+    $("p#reveal4").toggle();
+  });
+  $("p#show5").click(function() {
+    $("p#reveal5").toggle();
+  });
+  $("p#show6").click(function() {
+    $("p#reveal6").toggle();
+  });
+  $("p#show7").click(function() {
+    $("p#reveal7").toggle();
+  });
+  $("p#show8").click(function() {
+    $("p#reveal8").toggle();
+  });
+});
